4968c45ce9b6599b505ababa2c942ed2c3fb051d
Alex Withington-Smith <alex@awithsmith.com>
2021-03-31 18:38:39 +0100

    removed cors issues

--- src/app/services/accounts.service.ts
+++ src/app/services/accounts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Injectable } from '@angular/core';
 
@@ -7,8 +7,11 @@ export class AccountsService {
   constructor(private http: HttpClient) {}
 
   authenticate(email: string, password: string) {
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
     this.http
-      .post('http://localhost:4201/api/auth', { email, password })
+      .post('http://localhost:4201/api/auth', { email, password }, { headers })
       .subscribe((response) => {
         console.log(response);
       });
